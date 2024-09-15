52be2cf8e1c018874244ebd3ecad7c7a3ecf947a
antoniomf97 <antonio.mf97@gmail.com>
2024-09-15 16:04:07 +0100

    Added comments and formating

--- services/plotter/plotter.py
+++ services/plotter/plotter.py
@@ -11,44 +11,46 @@ def get_data(file_path):
     df = pd.read_csv(file_path, sep=",", index_col=0)
     return df
 
-def build_configs(configs):
+
+def frame_builder(configs):
     plt.title(configs.title)
     plt.xlabel(configs.xlabel)
     plt.ylabel(configs.ylabel)
     plt.grid(configs.grid)
 
 
-def build_plot(data):
+def plot_builder(data):
+    # here we are just considering one plot labeled y
+    # TODO: make it general for all plots
     plt.plot(data)
     plt.scatter(data.index, data.y)
 
 
-def build_graph(data, configs):
+def graph_builder(data, configs):
     fig = plt.figure()
 
-    build_configs(configs=configs)
-    build_plot(data=data)
+    # TODO: validator()
+    frame_builder(configs=configs)
+    plot_builder(data=data)
 
     buffer = BytesIO()
     fig.savefig(buffer, format="png")
 
     return buffer.getvalue()
 
+
 def run():
     # running from local file
     file_path = "../../tests/data/test.csv"
     configurations = Configurations(
-        title="This is the title",
-        xlabel="x axis",
-        ylabel="y axis",
-        plot_color="green"
+        title="This is the title", xlabel="x axis", ylabel="y axis", plot_color="green"
     )
 
     data = get_data(file_path)
-    response = build_graph(data, configurations)
+    response = graph_builder(data, configurations)
 
     image = Image.open(BytesIO(response))
-    image.save('./tmp/test.png')
+    image.save("./tmp/test.png")
 
 
 if __name__ == "__main__":
