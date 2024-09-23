02773d7d4a0e9b2540f4e3131412c4acab2ba890
antoniomf97 <antonio.mf97@gmail.com>
2024-09-23 23:12:26 +0100

    Reformated files

--- tests/integration/plotter_test.py
+++ tests/integration/plotter_test.py
@@ -8,9 +8,7 @@ def test_call_endpoint():
 
 
 def test_no_file_provided():
-    payload = {
-        "title": "This is a title"
-    }
+    payload = {"title": "This is a title"}
 
     response = r.post(url=config("PLOTTER_ENDPOINT"), params=payload)
     assert response.status_code == 400
@@ -20,9 +18,7 @@ def test_plot_2d():
     file_path = "./data/test.csv"
 
     files = {("file", open(file_path, "rb"))}
-    payload = {
-        "title": "This is a title"
-    }
+    payload = {"title": "This is a title"}
 
     response = r.post(url=config("PLOTTER_ENDPOINT"), files=files, params=payload)
     assert response.status_code == 200
